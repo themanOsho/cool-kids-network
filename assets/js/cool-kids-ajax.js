61b548fe2c512b200c737f127d2683acda6dc752
Joshua Osho <tlkjorsh@icloud.com>
2025-02-14 18:58:00 +0100

    Fix: Added file doc comment to cool-kids-ajax.js

--- assets/js/cool-kids-ajax.js
+++ assets/js/cool-kids-ajax.js
@@ -1,37 +1,34 @@
-jQuery( document ).ready(
-	function ($) {
-		$( '#cool-kids-login' ).on(
-			'submit',
-			function (e) {
-				e.preventDefault();
-				let email = $( '#email' ).val();
-				let nonce = $( '#cool_kids_nonce' ).val();
+/**
+ * Cool Kids Network - AJAX Login Handler
+ *
+ * This script handles the AJAX-based login functionality.
+ *
+ * @package CoolKidsNetwork
+ */
+jQuery(document).ready(function($) {
+    $('#cool-kids-login').on('submit', function(e) {
+        e.preventDefault();
+        let email = $('#email').val();
+        let nonce = $('#cool_kids_nonce').val();
 
-				$.ajax(
-					{
-						type: 'POST',
-						url: cool_kids_ajax.ajax_url,
-						data: {
-							action: 'cool_kids_login',
-							email: email,
-							nonce: nonce
-						},
-						success: function (response) {
-							if (response.success) {
-								$( '#login-message' ).html( '<p style="color: green;">' + response.data.message + '</p>' );
-								setTimeout(
-									function () {
-										window.location.href = cool_kids_ajax.redirect_url;
-									},
-									1000
-								);
-							} else {
-								$( '#login-message' ).html( '<p style="color: red;">Login failed. Please try again.</p>' );
-							}
-						}
-					}
-				);
-			}
-		);
-	}
-);
+        $.ajax({
+            type: 'POST',
+            url: cool_kids_ajax.ajax_url,
+            data: {
+                action: 'cool_kids_login',
+                email: email,
+                nonce: nonce
+            },
+            success: function(response) {
+                if (response.success) {
+                    $('#login-message').html('<p style="color: green;">' + response.data.message + '</p>');
+                    setTimeout(function() {
+                        window.location.href = cool_kids_ajax.redirect_url;
+                    }, 1000);
+                } else {
+                    $('#login-message').html('<p style="color: red;">Login failed. Please try again.</p>');
+                }
+            }            
+        });
+    });
+});
